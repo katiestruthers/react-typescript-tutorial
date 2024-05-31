9780777a44851839b319940337e0b5de2917d4e8
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-05-31 11:53:54 -0500

    event props

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,18 +1,14 @@
 import './App.css';
-import { Greet } from './components/Greet';
-import { Heading } from './components/Heading';
-import { Oscar } from './components/Oscar';
-import { Status } from './components/Status';
+import { Button } from './components/Button';
+import { Input } from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      <Status status='loading' />
-      <Heading>This is a heading</Heading>
-      <Oscar>
-        <Heading>Oscar goes to Leonardo!</Heading>
-      </Oscar>
-      <Greet name='Katie' isLoggedIn={true} />
+      <Button handleClick={(event, id) => {
+        console.log('Button clicked', event, id);
+      }}/>
+      <Input value='' handleChange={event => console.log(event)} />
     </div>
   );
 }
