ffd838077d8613a970caa4cdd5f56465fade9a4e
Katie Struthers <99215338+katiestruthers@users.noreply.github.com>
2024-06-01 17:42:59 -0500

    useContext hook

--- react-typescript/src/App.tsx
+++ react-typescript/src/App.tsx
@@ -1,8 +1,13 @@
 import './App.css';
+import { ThemeContextProvider } from './components/context/ThemeContext';
+import { Box } from './components/context/Box';
 
 function App() {
   return (
     <div className="App">
+      <ThemeContextProvider>
+        <Box />
+      </ThemeContextProvider>
     </div>
   );
 }
